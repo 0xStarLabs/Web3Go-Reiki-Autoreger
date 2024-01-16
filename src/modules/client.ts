852f9aa9ddb737aba2aa93d56fbaeb886c96c5ac
0xVoyager21 <129509129+0xVoyager21@users.noreply.github.com>
2024-01-16 16:37:16 +0200

    Update client.ts

--- src/modules/client.ts
+++ src/modules/client.ts
@@ -17,8 +17,6 @@ export class Client {
             retry: {
                 limit: 5,
                 methods: ['GET', 'POST', 'PUT', 'HEAD', 'DELETE'],
-                statusCodes: [400, 401, 403, 404, 405, 406, 407, 408, 413, 429, 500, 502, 503, 504],
-                errorCodes: ['ETIMEDOUT', 'ECONNRESET', 'EADDRINUSE', 'ECONNREFUSED', 'EPIPE', 'ENOTFOUND', 'ENETUNREACH', 'EAI_AGAIN']
             },
             headers: {
                 "accept": "application/json, text/plain, */*",
